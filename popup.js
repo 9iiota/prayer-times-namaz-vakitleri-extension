bebebb7c1b8b4c8481d1dc1989f7b95f8cbe1a1a
Burak Kilic <crazyshocklight@gmail.com>
2025-07-14 13:23:35 +0200

    Refactor styles and update popup behavior for prayer times extension

--- popup.js
+++ popup.js
@@ -1,32 +1,54 @@
 document.addEventListener("DOMContentLoaded", async () =>
 {
+    // fetch(`https://namazvakitleri.diyanet.gov.tr/tr-TR/13980`)
+    //     .then(response => response.text())
+    //     .then(data =>
+    //     {
+    //         console.log(data);
+    //         // const matches = [...data.matchAll(NAMAZ_TIME_REGEX)];
+    //         // if (matches.length !== 6)
+    //         // {
+    //         //     throw new Error('Failed to match namaz times due to an invalid city code');
+    //         // }
+
+    //         // const nextImsakTime = data.match(NEXT_IMSAK_TIME_REGEX)[1];
+    //         // let namazTimesFormatted = matches.map(match => match[1]);
+    //         // namazTimesFormatted.push(nextImsakTime);
+
+    //         // callback(namazTimesFormatted);
+    //     })
+    //     .catch(() =>
+    //     {
+    //         throw new Error('Failed to fetch namaz times');
+    //     });
+
     const nextTimeDiv = document.getElementById("next-time");
     nextTimeDiv.addEventListener("click", () =>
     {
-        nextTimeDiv.style.backgroundColor = nextTimeDiv.style.backgroundColor === "rgb(173, 216, 230)" ? "#f0f0f0" : "#add8e6ff";
+        nextTimeDiv.style.backgroundColor = nextTimeDiv.style.backgroundColor === "rgb(173, 216, 230)" ? "#f4dfb4" : "#add8e6ff";
     });
 
     const container = document.querySelector(".container");
     const totalPrayers = 7;
-    const nextPrayerIndex = 4;
+    const nextPrayerIndex = 7;
 
     for (let i = 0; i < totalPrayers - 1; i++)
     {
+        const absoluteDiff = i < nextPrayerIndex ? nextPrayerIndex - i : i - nextPrayerIndex + 1;
         const div = document.createElement("div");
         div.className = "stacked";
         div.textContent = `Prayer ${i + 1}`;
-        const absoluteDiff = Math.abs(i - nextPrayerIndex);
+        div.style.width = `${75 - absoluteDiff * 7}vw`;
+        div.style.filter = `brightness(${100 - absoluteDiff * 10}%)`;
 
         if (i < nextPrayerIndex)
         {
-            div.style.width = `${45 - (absoluteDiff) * 5}vw`;
-            div.style.top = `${50 - (absoluteDiff) * 6}vh`;
+            div.style.top = `${50 - absoluteDiff * 5}%`;
             container.insertBefore(div, nextTimeDiv);
         }
         else
         {
-            div.style.width = `${45 - (absoluteDiff + 1) * 5}vw`;
-            div.style.top = `${50 + (absoluteDiff + 1) * 6}vh`;
+            div.style.top = `${50 + absoluteDiff * 5}vh`;
             container.appendChild(div);
         }
     }
